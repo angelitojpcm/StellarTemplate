9f1196d4b9f7b8d87dd45f5e33067612511f28de
Angel Jean Pierre Calderon Mantilla <angelitojpcmantilla24@gmail.com>
2024-06-05 00:58:15 -0500

    feat: Add testimonials functionality and update testimonials section

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,51 +27,9 @@
         (this.sideNav = $(".rbt-search-dropdown"));
     },
     methods: function (e) {
-      this.dont();
       this.topBar();
       this.scrollUp();
-      this.pricing();
-    },
-    dont: function (e) {
-      $("body").on("contextmenu", function (e) {
-        Toast.fire({
-          icon: "error",
-          title: "Oops...",
-          text: "Right click is disabled",
-          animation: true,
-          customClass: {
-            popup: "animated tada",
-          },
-        });
-        return false;
-      });
-      $("body").on("selectstart", function (e) {
-        Toast.fire({
-          icon: "error",
-          title: "Oops...",
-          text: "Text selection is disabled",
-          animation: true,
-          customClass: {
-            popup: "animated tada",
-          },
-        });
-        return false;
-      });
-      $("body").on("click", function (e) {
-        if (e.target.tagName == "IMG") {
-          Toast.fire({
-            icon: "error",
-            title: "Oops...",
-            text: "Image is disabled",
-            animation: true,
-            customClass: {
-              popup: "animated tada",
-            },
-          });
-          return false;
-        }
-        return false;
-      });
+      this.testimonials();
     },
     topBar: function (e) {
       let topbar = $("header .topbar");
@@ -104,8 +62,65 @@
         return false;
       });
     },
-    animation_bar: function (e) {},
-    pricing: function (e) {},
+    testimonials: function () {
+      var testimonials = $(".testimonial");
+      var container = $(".testimonials-container");
+      var currentIndex = 0;
+      var visibleTestimonials = 1;
+
+      function showTestimonials() {
+        testimonials.hide();
+        for (
+          var i = currentIndex;
+          i < currentIndex + visibleTestimonials;
+          i++
+        ) {
+          testimonials.eq(i).fadeIn(300);
+        }
+      }
+
+      function updateButtons() {
+        $(".prev-btn").prop("disabled", currentIndex === 0);
+        $(".next-btn").prop(
+          "disabled",
+          currentIndex + visibleTestimonials >= testimonials.length
+        );
+      }
+
+      $(window)
+        .on("resize", function () {
+          var width = $(window).width();
+
+          if (width < 600) {
+            visibleTestimonials = 1;
+          } else if (width < 1000) {
+            visibleTestimonials = 2;
+          } else {
+            visibleTestimonials = 4;
+          }
+          currentIndex = 0; 
+          showTestimonials();
+          updateButtons();
+        })
+        .trigger("resize"); 
+
+      $(".prev-btn").on("click", function () {
+        if (currentIndex > 0) {
+          currentIndex -= visibleTestimonials;
+          if (currentIndex < 0) currentIndex = 0; 
+          showTestimonials();
+          updateButtons();
+        }
+      });
+
+      $(".next-btn").on("click", function () {
+        if (currentIndex + visibleTestimonials < testimonials.length) {
+          currentIndex += visibleTestimonials;
+          showTestimonials();
+          updateButtons();
+        }
+      });
+    },
   };
 
   app.i();
